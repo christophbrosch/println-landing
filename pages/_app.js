4522e72b4d52a118be83dcbbc99d6d1e62a8f664
Christoph Brosch <christoph.brosch@outlook.de>
2022-03-04 20:36:34 +0100

    inital landing page design implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+
+// Bootstrap imported from package
+import "../styles/bootstrap.scss"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  // https://www.kindacode.com/article/how-to-correctly-use-bootstrap-5-in-next-js/
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
   return <Component {...pageProps} />
 }
 
