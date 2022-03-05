b87b4a586a312371693ebe3c075c30f4343783ab
Christoph Brosch <christoph.brosch@outlook.de>
2022-03-05 11:05:04 +0100

    pushed env into vercel, therefore we seperate beween dev and prod now

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,31 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 import Typist from "react-typist"
-import { Container, Navbar, Nav, Row, Col, Button } from 'react-bootstrap'
+import { Container, Navbar, Nav, Row, Col, Button, Tooltip, OverlayTrigger } from 'react-bootstrap'
 import styles from '../styles/Index.module.scss'
-import { delBasePath } from 'next/dist/shared/lib/router/router'
 
-import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs, where, query} from 'firebase/firestore/lite';
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
+import { initializeApp } from 'firebase/app'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCU9YGCgqgSh5dUAxNnc0vmrJP5GvbPwDU",
-  authDomain: "println-it.firebaseapp.com",
-  projectId: "println-it",
-  storageBucket: "println-it.appspot.com",
-  messagingSenderId: "819476371230",
-  appId: "1:819476371230:web:7f85d5c674cd48d9255b0f",
-  measurementId: "G-NXVLGKE3RX"
-};
+import { validateEmail } from '../lib/utils'
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-function validateEmail(email) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
-  {
-    return (true)
-  }
-    return (false)
-}
+export default function Home({firebaseConfig}) {
+  const app = initializeApp(firebaseConfig)
+  const db = getFirestore(app)
 
-export default function Home() {
-
-  
   async function handleNewsletterSubscription(e) {
     e.preventDefault()
-    console.log(process.env)
     const email = e.target[0].value 
     const newsletterCol = collection(db, "newsletter")
 
     if (validateEmail(email)) {
       const q = query(newsletterCol, where("email", "==", email))
       const d = await getDocs(q)
-      
+
       if (d.size == 0) {
         addDoc(newsletterCol, {
           active: true,
-          test: true,
           email: email,
           timestamp: Date.now()
         })
@@ -62,6 +38,12 @@ export default function Home() {
     }
   }
 
+  const renderTooltip = (props) => (
+    <Tooltip {...props}>
+    Which we do not have.
+    </Tooltip>
+  );
+
   return (
     <>
       <Head>
@@ -71,7 +53,14 @@ export default function Home() {
       </Head>
       <section className={styles.layout__herosection}>
         <div className="mb-0 alert alert-primary d-flex justify-content-center">
-          Come talk to us on social media!  
+          Come talk to us on&nbsp;
+          <OverlayTrigger
+            placement="bottom"
+            delay={{ show: 250, hide: 400 }}
+            overlay={renderTooltip}
+          >
+            <span>social media!</span>
+          </OverlayTrigger>  
         </div>
         <Navbar expand="lg">
           <Container>
@@ -89,7 +78,7 @@ export default function Home() {
         <div className={styles.herosection__pages}>
           <Container>
             <Row className="h-100">
-              <Col xs={6}>
+              <Col xs={12} md={6}>
                 <h1 className="fw-bold text-white"> Print it into the cloud, read it everywhere. </h1>
                 <p className="text-light">Println aims to help developers stay up to date with what their software is doing, whether it runs in the cloud, or not.</p>
               </Col>
@@ -125,4 +114,20 @@ export default function Home() {
       </section>
     </>
   )
+}
+
+Home.getInitialProps = async (ctx) => {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.FIREBASE_APPID
+  };
+
+  if (process.env.FIREBASE_MEASUREMENTID != undefined) {
+    firebaseConfig["measurementId"] = process.env.FIREBASE_MEASUREMENTID
+  }
+  return {firebaseConfig: firebaseConfig}
 }
